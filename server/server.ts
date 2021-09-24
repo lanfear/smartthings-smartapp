af64c1a1a33cb62a099a9994c59ae5ec4c412f80
Joseph M. Deming <j.deming@techsmith.com>
2021-09-24 18:14:25 -0400

    server can start

---
+++ server/server.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line
+//require('dotenv').config();
+
+import express from 'express'
+import smartApp from './smartapp'
+
+
+const server = express();
+const PORT = process.env.PORT || 9788;
+
+/* Configure Express to handle JSON lifecycle event calls from SmartThings */
+server.use(express.json());
+server.post('/', (req, res) => {
+    void smartApp.handleHttpCallback(req, res);
+});
+
+server.get('/oauth', (req, res) => {
+    console.log(JSON.stringify(req.query))
+    res.send("OK")
+})
+
+/* Start listening at your defined PORT */
+server.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
---
+++ server/smartapp.ts
@@ -0,0 +1,44 @@
+import { SmartApp } from '@smartthings/smartapp';
+
+const smartApp = new SmartApp()
+	.enableEventLogging(2) // logs all lifecycle event requests/responses as pretty-printed JSON. Omit in production
+	.configureI18n()
+
+	// Configuration page definition
+	.page('mainPage', (_, page) => {
+
+		// prompts user to select a contact sensor
+		page.section('sensors', section => {
+			section
+				.deviceSetting('contactSensor')
+				.capabilities(['contactSensor'])
+				.required(true)
+		})
+
+		// prompts users to select one or more switch devices
+		page.section('lights', section => {
+			section
+				.deviceSetting('lights')
+				.capabilities(['switch'])
+				.required(true)
+				.multiple(true)
+				.permissions('rx')
+		})
+	})
+
+	// Handler called whenever app is installed or updated
+	// Called for both INSTALLED and UPDATED lifecycle events if there is
+	// no separate installed() handler
+	.updated(async (context) => {
+		await context.api.subscriptions.delete()
+		await context.api.subscriptions.subscribeToDevices(context.config.contactSensor,
+			'contactSensor', 'contact', 'openCloseHandler')
+	})
+
+	// Handler called when the configured open/close sensor opens or closes
+	.subscribedEventHandler('openCloseHandler', async (context, event) => {
+		const value = event.value === 'open' ? 'on' : 'off'
+		await context.api.devices.sendCommands(context.config.lights, 'switch', value)
+	})
+
+export default smartApp
