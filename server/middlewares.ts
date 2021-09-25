24749d013ac639295a6ecee0f2d14899fd833c57
Joseph M. Deming <j.deming@techsmith.com>
2021-09-24 21:15:19 -0400

    server appears to be running with all features

---
+++ server/middlewares.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+
+function notFound(req: Request, res: Response, next: Function) {
+    if (process.env.REACT_APP) next(); // In order to show custom error 404 page
+    else {
+        res.status(404);
+        const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
+        next(error);
+    }
+}
+
+function errorHandler(err: Error, req: Request, res: Response, next: Function) {
+    const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack
+    });
+}
+
+module.exports = {
+    notFound,
+    errorHandler
+};
--- server/server.ts
+++ server/server.ts
@@ -1,23 +1,106 @@
 // eslint-disable-next-line
+import { SceneSummary, Device, Rule } from '@smartthings/core-sdk';
+import express from 'express';
+import path from 'path';
+import process from './provider/env';
+import smartApp from './provider/smartapp';
+import db from './provider/db';
+import sse from './provider/sse';
 //require('dotenv').config();
-
-import express from 'express'
-import smartApp from './smartapp'
-
-
 const server = express();
-const PORT = process.env.PORT || 9190;
+const PORT = process.env.PORT || 3001;
 
-/* Configure Express to handle JSON lifecycle event calls from SmartThings */
 server.use(express.json());
-server.post('/', (req, res) => {
+// server.use(express.static(path.join(__dirname, '../public')));
+// server.set('views', path.join(__dirname, '../views'))
+// server.set('view engine', 'ejs')
+
+/* Handle lifecycle event calls from SmartThings */
+server.post('/smartapp', (req, res) => {
     void smartApp.handleHttpCallback(req, res);
 });
 
-server.get('/oauth', (req, res) => {
-    console.log(JSON.stringify(req.query))
-    res.send("OK")
+/**
+ * Render the home page listing installed app instances
+ */
+server.get('/', (req, res) => {
+    const installedAppIds = db.listInstalledApps()
+    res.render('index', {installedAppIds})
 })
 
+/**
+ * Render the installed app instance control page
+ */
+// would be neat to fix this, but appears handler for express cannot be async... but this functions as expected
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+server.get('/isa/:id', async (req, res) => {
+    const context = await smartApp.withContext(req.params.id)
+
+    const options:{ installedAppId: string, scenes: SceneSummary[], switches: Device[], locks: Device[] } = {
+        installedAppId: req.params.id,
+        scenes: [],
+        switches: [],
+        locks: []
+    }
+
+    if (context.configBooleanValue('scenes')) {
+        // @ts-ignore
+        options.scenes = await context.api.scenes.list()
+    }
+
+    if (context.configBooleanValue('switches')) {
+        // @ts-ignore
+        options.switches = await Promise.all((await context.api.devices.list({capability: 'switch'})).map(async it => {
+            // @ts-ignore
+            const state = await context.api.devices.getCapabilityStatus(it.deviceId, 'main', 'switch');
+            return {
+                deviceId: it.deviceId,
+                label: it.label,
+                value: state.switch.value
+            };
+        }))
+    }
+
+    if (context.configBooleanValue('locks')) {
+        // @ts-ignore
+        options.locks = await Promise.all((await context.api.devices.list({capability: 'lock'})).map(async it => {
+            // @ts-ignore
+            const state = await context.api.devices.getCapabilityStatus(it.deviceId, 'main', 'lock');
+            return {
+                deviceId: it.deviceId,
+                label: it.label,
+                value: state.lock.value
+            };
+        }))
+    }
+
+    res.render('isa', options)
+})
+
+/* Execute a scene */
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+server.post('/isa/:id/scenes/:sceneId', async (req, res) => {
+    const context = await smartApp.withContext(req.params.id)
+    const result = await context.api.scenes.execute(req.params.sceneId)
+    res.send(result)
+});
+
+/* Execute a device command */
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+server.post('/isa/:id/devices/:deviceId', async (req, res) => {
+    const context = await smartApp.withContext(req.params.id)
+    const result = await context.api.devices.executeCommand(req.params.deviceId, req.body)
+    res.send(result)
+});
+
+/**
+ * Handle SSE connection from the web page
+ */
+server.get('/events', sse.init);
+
 /* Start listening at your defined PORT */
-server.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
+server.listen(PORT, () => {
+    console.log(`Server is up and running at http://localhost:${PORT}`)
+});
+
+export default server;
