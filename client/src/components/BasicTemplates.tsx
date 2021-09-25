fd1d39d3435c8f84cb83b6688bd702aaf3bd57e3
Joseph M. Deming <j.deming@techsmith.com>
2021-09-24 22:17:28 -0400

    move client app to typescript

--- client/src/components/BasicTemplates.tsx
+++ client/src/components/BasicTemplates.tsx
@@ -1,25 +1,25 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-json";
 import "ace-builds/src-noconflict/theme-monokai";
 import "../App.scss";
-// Rules
-import Examples from "../templates/ruleExamples";
+// Rule Templates
+import { BasicTemp } from "../templates/ruleTemplates";
 
-function RuleExamples() {
+function BasicTemplates() {
   const [rule, setRule] = useState("{}");
   return (
     <div className="App">
       <div className="container">
-        <div className="columns">
-          <div className="column">
-              <aside className="menu left-menu">
-                <ul className="menu-list">
-                  {Examples.map((rule) => {
-                    return <li><button className="button" onClick={() => setRule(JSON.stringify(rule.value, null, 2))}>{rule.label}</button></li>
-                  })}
-                </ul>
-              </aside>
+        <div className="columns is-mobile">
+          <div className="column is-one-third">
+            <div className="left-menu">
+              <div className="block">
+                {BasicTemp.map((rule) => {
+                  return <button className="button" onClick={() => setRule(JSON.stringify(rule.value, null, 2))}>{rule.label}</button>
+                })}
+              </div>
+            </div>
           </div>
           <div className="column">
             <div id="example">
@@ -31,8 +31,8 @@ function RuleExamples() {
                 name="textarea"
                 value={rule}
                 editorProps={{ $blockScrolling: true }}
-                setOptions={{ fontSize: 15 }}
-                />
+	              setOptions={{ fontSize: 15 }}
+              />
             </div>
           </div>
         </div>
@@ -41,4 +41,4 @@ function RuleExamples() {
   );
 };
 
-export default RuleExamples;
+export default BasicTemplates;
