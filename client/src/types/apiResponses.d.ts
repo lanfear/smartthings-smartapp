7138697377f805d65cb45d2165c0c1f706adeb20
Joseph M. Deming <j.deming@techsmith.com>
2021-09-26 21:27:45 -0400

    add types for SmartApp api responses

---
+++ client/src/types/apiResponses.d.ts
@@ -0,0 +1,22 @@
+// could get this from @smartthings/core-sdk but to bring it all in for types
+// and nothing really promises contract alignment between smartapp and smartthings api
+export interface ISceneResponse {
+    sceneId: string,
+    sceneName: string,
+    sceneIcon: string,
+    sceneColor: string,
+    locationId: string,
+    createdBy: string,
+    createdDate: Date,
+    lastUpdatedDate: Date,
+    lastExecutedDate: Date,
+    editable: boolean,
+    apiVersion: string
+
+}
+
+export interface IDeviceResponse {
+    deviceId: string,
+    label: string,
+    value: string  // 'on' or 'off'?
+}
