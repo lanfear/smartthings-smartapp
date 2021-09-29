34ebee131a93dacf2d06d4390bfdcd55e35670cb
Joseph M. Deming <j.deming@techsmith.com>
2021-09-28 22:20:28 -0400

    turns out i had everything i needed for the sake of listing installed apps

--- client/src/operations/getInstalledSmartApps.ts
+++ client/src/operations/getInstalledSmartApps.ts
@@ -1,11 +1,6 @@
-const getInstalledSmartApps = (): IResponseSmartApps => {
-    // TODO
-    // const response = await fetch(`isa/${isaId}`);
-    // const responseBody = await response.json();
-    // return responseBody;
-    return [
-        'a087c8de-a8ef-46df-8069-1b2e9fa4f6d9'
-    ];
+const getInstalledSmartApps = async () => {
+    const response = await fetch('http://localhost:9190/app');
+    return await response.json() as IResponseSmartApps;
 };
 
 export type IResponseSmartApps = string[];
