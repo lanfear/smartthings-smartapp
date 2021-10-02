af6d642ab9039c64b60f5cc891899801751139f8
Joseph M. Deming <j.deming@techsmith.com>
2021-10-02 15:27:34 -0400

    add simple-json-db as a simple db store

--- server/provider/db.ts
+++ server/provider/db.ts
@@ -2,8 +2,10 @@
 
 import fs from 'fs'
 const dataDirectory = process.env.DATA_DIRECTORY || 'data'
+const ruleStore = process.env.DAT_RULESTORE || 'rules.json';
 
 export default {
 	listInstalledApps: (): string[] => fs.readdirSync(dataDirectory).map((it: string) => it.substring(0, it.length - 5)),
-	dataDirectory
+	dataDirectory,
+	ruleStorePath: `${dataDirectory}/${ruleStore}`
 }
