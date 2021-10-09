9c7adf87b8f350419d5c67ab82c4a6d264eafe76
Joseph M. Deming <j.deming@techsmith.com>
2021-10-08 21:29:18 -0400

    added factory to get unique device contexts from overlapping arrays

---
+++ server/factories/uniqueDeviceFactory.ts
@@ -0,0 +1,5 @@
+import {DeviceContext} from '@smartthings/smartapp';
+
+const create = (devices: DeviceContext[]): DeviceContext[] => devices.filter((s, i, self) => self.findIndex(c => c.deviceId === s.deviceId) === i);
+
+export default create;
