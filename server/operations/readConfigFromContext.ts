85c5efe2e1b2c88b5adf507fe0d433845fbf56cf
Joe Deming <joe@josephdeming.net>
2021-10-12 22:27:38 -0400

    much more to guard in new technique of parsing config

--- server/operations/readConfigFromContext.ts
+++ server/operations/readConfigFromContext.ts
@@ -3,22 +3,22 @@ import {SmartAppContext} from '@smartthings/smartapp';
 import {ISmartAppRuleConfigValues} from '../types';
 
 const readConfigFromContext = async (context: SmartAppContext): Promise<ISmartAppRuleConfigValues> => ({
-  enableAllRules: context.configBooleanValue('enableAllRules').valueOf(),
-  enableDaylightRule: context.configBooleanValue('enableDaylightRule').valueOf(),
-  enableNightlightRule: context.configBooleanValue('enableNightlightRule').valueOf(),
-  enableIdleRule: context.configBooleanValue('enableIdleRule').valueOf(),
-  motionSensors: await context.configDevices('motionSensor') || [],
-  motionMultipleAll: context.configBooleanValue('motionMultipleAll').valueOf(),
-  motionIdleTimeout: context.configNumberValue('motionIdleTimeout').valueOf(),
+  enableAllRules: context.configBooleanValue('enableAllRules')?.valueOf(),
+  enableDaylightRule: context.configBooleanValue('enableDaylightRule')?.valueOf(),
+  enableNightlightRule: context.configBooleanValue('enableNightlightRule')?.valueOf(),
+  enableIdleRule: context.configBooleanValue('enableIdleRule')?.valueOf(),
+  motionSensors: context.isAuthenticated() && await context.configDevices('motionSensor') || [],
+  motionMultipleAll: context.configBooleanValue('motionMultipleAll')?.valueOf(),
+  motionIdleTimeout: context.configNumberValue('motionIdleTimeout')?.valueOf(),
   motionIdleTimeoutUnit: context.configBooleanValue('motionIdleTimeoutUnit') ? IntervalUnit.Minute : IntervalUnit.Second,
-  motionDurationDelay: context.configNumberValue('motionDurationDelay').valueOf(),
-  dayControlSwitch: (await context.configDevices('dayControlSwitch'))[0],
-  dayActiveSwitches: await context.configDevices('dayActiveSwitches') || [],
-  nightControlSwitch: (await context.configDevices('nightControlSwitch'))[0],
-  nightActiveSwitches: await context.configDevices('nightActiveSwitches') || [],
-  dayNightOffset: context.configNumberValue('dayNightOffset').valueOf(),
-  nightEndOffset: context.configNumberValue('nightEndOffset').valueOf(),
-  dayStartOffset: context.configNumberValue('dayStartOffset').valueOf()
+  motionDurationDelay: context.configNumberValue('motionDurationDelay')?.valueOf(),
+  dayControlSwitch: (context.isAuthenticated() && await context.configDevices('dayControlSwitch') || [null])[0],
+  dayActiveSwitches: context.isAuthenticated() && await context.configDevices('dayActiveSwitches') || [],
+  nightControlSwitch: (context.isAuthenticated() && await context.configDevices('nightControlSwitch') || [null])[0],
+  nightActiveSwitches: context.isAuthenticated() && await context.configDevices('nightActiveSwitches') || [],
+  dayNightOffset: context.configNumberValue('dayNightOffset')?.valueOf(),
+  nightEndOffset: context.configNumberValue('nightEndOffset')?.valueOf(),
+  dayStartOffset: context.configNumberValue('dayStartOffset')?.valueOf()
 });
 
 export default readConfigFromContext;
