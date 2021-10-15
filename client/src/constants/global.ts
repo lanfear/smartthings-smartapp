3128b6d3f348792d0d2aab938c84399180c93aad
Joe Deming <joe@josephdeming.net>
2021-10-15 15:48:17 -0400

    delete the client side of code that is na to a pure smartapp

--- client/src/constants/global.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  rule: {
-    default: {
-      switchLevelRate: 20
-    }
-  }
-};
--- client/src/operations/getInstalledSmartApps.ts
+++
@@ -1,8 +0,0 @@
-const getInstalledSmartApps = async (): Promise<IResponseSmartApps> => {
-  const response = await fetch('http://localhost:9190/app');
-  return await response.json() as IResponseSmartApps;
-};
-
-export type IResponseSmartApps = string[];
-
-export default getInstalledSmartApps;
