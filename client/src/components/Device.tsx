26108598f807284d4d3fb6bb65780beba494228f
Joe Deming <joe@josephdeming.net>
2021-11-16 22:45:25 -0500

    more fun with grid and flex

--- client/src/components/Device.tsx
+++ client/src/components/Device.tsx
@@ -1,126 +1,100 @@
-import React, {useState} from 'react';
-import {Popover} from 'react-tiny-popover';
+import React from 'react';
 import styled from 'styled-components';
 import {IDevice} from '../types/smartthingsExtensions';
 
-const DeviceTitle = styled.div`
-    font-size: larger;
-    font-weight: 700;
-`;
-
 const DeviceStatus = styled.div`
-    font-size: smaller;
-    font-weight: 500;
+  font-size: smaller;
+  font-weight: 500;
 `;
 
 const DeviceIcon = styled.div`
-    font-size: larger;
+  font-size: larger;
 `;
 
 const DeviceContainer = styled.button`
-    min-width: 3rem;
-    min-height: 3rem;
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-    align-content: center;
-    align-items: center;
-    justify-content: space-evenly;
-    border: 1px solid gray;
-    border-radius: 4px;
+  height: 3rem;
+  width: 3rem;
+  display: flex;
+  flex: none;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
+  justify-content: space-evenly;
+  border: 1px solid gray;
+  border-radius: 4px;
 `;
 
 const LightContainer = styled(DeviceContainer) <{ isLightOn: boolean }>`
-    ${props => props.isLightOn ? `
-    box-shadow:
-        0px 0px 10px 2px yellow, 
-        inset 0px 0px 20px 15px yellow;
-    ` : ''}
+  ${props => props.isLightOn ? `
+  box-shadow:
+      0px 0px 10px 2px yellow, 
+      inset 0px 0px 20px 15px yellow;
+  ` : ''}
 `;
 
 const MotionContainer = styled(DeviceContainer) <{ isActive: boolean }>`
-    ${props => props.isActive ? `
-    box-shadow:
-        0px 0px 10px 2px lightgreen, 
-        inset 0px 0px 20px 15px lightgreen;
-    ` : ''}
+  ${props => props.isActive ? `
+  box-shadow:
+      0px 0px 10px 2px lightgreen, 
+      inset 0px 0px 20px 15px lightgreen;
+  ` : ''}
 `;
 
-const Device: React.FC<IDeviceProps> = ({device, deviceType}) => {
-  const [popoverOpen, setPopoverOpen] = useState(false);
-
-  const deviceComponent = deviceType === 'Switch' ? (
-    <LightContainer
-      onMouseEnter={() => setPopoverOpen(true)}
-      onMouseLeave={() => setPopoverOpen(false)}
-      onTouchStart={() => setPopoverOpen(true)}
-      onTouchEnd={() => setPopoverOpen(false)}
-      isLightOn={device.value === 'on'}
-    >
-      {/* <span>{t('dashboard.switch.header.deviceId')}: {device.deviceId}</span> */}
-      <DeviceIcon>
-        💡
-      </DeviceIcon>
-      <DeviceStatus>
-        {device.value}
-      </DeviceStatus>
-    </LightContainer>
-  ) : deviceType === 'Lock' ? (
-    <DeviceContainer
-      onMouseEnter={() => setPopoverOpen(true)}
-      onMouseLeave={() => setPopoverOpen(false)}
-      onTouchStart={() => setPopoverOpen(true)}
-      onTouchEnd={() => setPopoverOpen(false)}
-    >
-      {/* <span>{t('dashboard.lock.header.deviceId')}: {device.deviceId}</span> */}
-      <DeviceIcon>
-        {device.value === 'locked' ? '🔒' : '🔓'}
-      </DeviceIcon>
-      <DeviceStatus>
-        {device.value}
-      </DeviceStatus>
-    </DeviceContainer>
-  ) : (
-    <MotionContainer
-      onMouseEnter={() => setPopoverOpen(true)}
-      onMouseLeave={() => setPopoverOpen(false)}
-      onTouchStart={() => setPopoverOpen(true)}
-      onTouchEnd={() => setPopoverOpen(false)}
-      isActive={device.value === 'active'}
-    >
-      {/* <span>{t('dashboard.motion.header.deviceId')}: {device.deviceId}</span> */}
-      <DeviceIcon>
-        {device.value === 'active' ? '🏃' : '🧍'}
-      </DeviceIcon>
-      <DeviceStatus>
-        {device.value}
-      </DeviceStatus>
-    </MotionContainer>
-  );
-
-  return (
-    <Popover
-      isOpen={popoverOpen}
-      positions={['top', 'left']} // if you'd like, you can limit the positions
-      padding={10} // adjust padding here!
-      reposition={false} // prevents automatic readjustment of content position that keeps your popover content within its parent's bounds
-      onClickOutside={() => setPopoverOpen(false)} // handle click events outside of the popover/target here!
-      content={() => (
-        <DeviceTitle>
-          {device.label}
-        </DeviceTitle>
-      )}
-    >
-      {deviceComponent}
-    </Popover>
-  );
-};
+const Device: React.FC<IDeviceProps> = ({device, deviceType, setActiveDevice}) => deviceType === 'Switch' ? (
+  <LightContainer
+    onMouseEnter={() => setActiveDevice(device)}
+    onMouseLeave={() => setActiveDevice(null)}
+    onTouchStart={() => setActiveDevice(device)}
+    onTouchEnd={() => setActiveDevice(null)}
+    isLightOn={device.value === 'on'}
+  >
+    {/* <span>{t('dashboard.switch.header.deviceId')}: {device.deviceId}</span> */}
+    <DeviceIcon>
+      💡
+    </DeviceIcon>
+    <DeviceStatus>
+      {device.value}
+    </DeviceStatus>
+  </LightContainer>
+) : deviceType === 'Lock' ? (
+  <DeviceContainer
+    onMouseEnter={() => setActiveDevice(device)}
+    onMouseLeave={() => setActiveDevice(null)}
+    onTouchStart={() => setActiveDevice(device)}
+    onTouchEnd={() => setActiveDevice(null)}
+  >
+    {/* <span>{t('dashboard.lock.header.deviceId')}: {device.deviceId}</span> */}
+    <DeviceIcon>
+      {device.value === 'locked' ? '🔒' : '🔓'}
+    </DeviceIcon>
+    <DeviceStatus>
+      {device.value}
+    </DeviceStatus>
+  </DeviceContainer>
+) : (
+  <MotionContainer
+    onMouseEnter={() => setActiveDevice(device)}
+    onMouseLeave={() => setActiveDevice(null)}
+    onTouchStart={() => setActiveDevice(device)}
+    onTouchEnd={() => setActiveDevice(null)}
+    isActive={device.value === 'active'}
+  >
+    {/* <span>{t('dashboard.motion.header.deviceId')}: {device.deviceId}</span> */}
+    <DeviceIcon>
+      {device.value === 'active' ? '🏃' : '🧍'}
+    </DeviceIcon>
+    <DeviceStatus>
+      {device.value}
+    </DeviceStatus>
+  </MotionContainer>
+);
 
 export type DeviceType = 'Switch' | 'Lock' | 'Motion';
 
 export interface IDeviceProps {
   device: IDevice;
   deviceType: DeviceType;
+  setActiveDevice: (value: IDevice|null) => void;
 }
 
 export default Device;
--- client/src/components/Room.tsx
+++ client/src/components/Room.tsx
@@ -3,66 +3,119 @@ import React from 'react';
 import styled from 'styled-components';
 import {useLocalStorage} from 'use-hooks';
 import {IResponseSmartApp} from '../operations/getInstalledSmartApp';
+import {IDevice} from '../types/smartthingsExtensions';
 import Device from './Device';
-
-const RoomTitle = styled.div`
-    font-size: larger;
-    font-weight: 700;
-`;
+import Power from './Power';
 
 const RoomContainer = styled.div`
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-    align-content: center;
-    align-items: center;
-    justify-content: flex-start;
-    border: 1px solid gray;
-    border-radius: 4px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
+  justify-content: flex-start;
+  border: 1px solid gray;
+  border-radius: 4px;
 `;
 
-const RoomDeviceGrid = styled.div`
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    gap: 2px;
-    grid-auto-columns: 1fr;
-    grid-auto-rows: 1fr;
+const RoomControlGrid = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: max-content 1fr 2rem;
+  gap: 2px;
+`;
+
+const RoomControlPower = styled.span`
+  display: flex;
+  grid-column-start: 1;
+  grid-column-end: 1;
+  grid-row-start: 1;
+  grid-row-end: 1;
+`;
+
+const RoomControlTitle = styled.span`
+  width: 100%;
+  display: flex;
+  grid-column-start: 2;
+  grid-column-end: 6;
+  grid-row-start: 1;
+  grid-row-end: 1;
+  justify-content: center;
+  align-items: center;
+  font-size: larger;
+  font-weight: 700;
+`;
+
+const RoomControlDevices = styled.span`
+  display: flex;
+  grid-column-start: 1;
+  grid-column-end: 6;
+  grid-row-start: 2;
+  grid-row-end: 2;
+  flex-wrap: wrap;
+`;
+
+const RoomControlDeviceLabel = styled.span`
+  display: flex;
+  grid-column-start: 1;
+  grid-column-end: 6;
+  grid-row-start: 3;
+  grid-row-end: 3;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Room: React.FC<IRoomProps> = ({room}) => {
   const [dashboardData] = useLocalStorage('smartAppState', {} as IResponseSmartApp);
+  const [activeDevice, setActiveDevice] = useLocalStorage(`smartAppRoom-${room.roomId as string}-activeDevice`, null as IDevice|null);
   const roomSwitches = dashboardData.switches.filter(d => d.roomId === room.roomId);
   const roomLocks = dashboardData.locks.filter(d => d.roomId === room.roomId);
   const roomMotion = dashboardData.motion.filter(d => d.roomId === room.roomId);
 
   return (
     <RoomContainer>
-      <RoomTitle>
-        {room.name}
-      </RoomTitle>
-      <RoomDeviceGrid>
-        {roomSwitches.map(s => (
-          <Device
-            key={`switch-${s.deviceId as string}`}
-            device={s}
-            deviceType="Switch"
+      <RoomControlGrid>
+        <RoomControlPower>
+          <Power
+            room={room}
+            isPowerOn={true}
           />
-        ))}
-        {roomLocks.map(s => (
-          <Device
-            key={`lock-${s.deviceId as string}`}
-            device={s}
-            deviceType="Lock"
-          />
-        ))}
-        {roomMotion.map(s => (
-          <Device
-            key={`motion-${s.deviceId as string}`}
-            device={s}
-            deviceType="Motion"
-          />
-        ))}
-      </RoomDeviceGrid>
+        </RoomControlPower>
+        <RoomControlTitle>
+          {room.name}
+        </RoomControlTitle>
+        <RoomControlDevices>
+          {roomSwitches.map(s => (
+            <Device
+              key={`switch-${s.deviceId as string}`}
+              device={s}
+              deviceType="Switch"
+              setActiveDevice={setActiveDevice}
+            />
+          ))}
+          {roomLocks.map(s => (
+            <Device
+              key={`lock-${s.deviceId as string}`}
+              device={s}
+              deviceType="Lock"
+              setActiveDevice={setActiveDevice}
+            />
+          ))}
+          {roomMotion.map(s => (
+            <Device
+              key={`motion-${s.deviceId as string}`}
+              device={s}
+              deviceType="Motion"
+              setActiveDevice={setActiveDevice}
+            />
+          ))}
+        </RoomControlDevices>
+        <RoomControlDeviceLabel>
+          {activeDevice?.label}
+        </RoomControlDeviceLabel>
+      </RoomControlGrid>
     </RoomContainer>
   );
 };
