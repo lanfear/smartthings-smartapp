861b200693f026fdc7799128a04d15b659ba54ae
Joe Deming <joe@josephdeming.net>
2022-03-28 21:06:34 -0400

    add sharedContract for rules event

--- client/src/types/sharedContracts.d.ts
+++ client/src/types/sharedContracts.d.ts
@@ -1,10 +1,14 @@
 // types in this file should be copied to corresponding file in server directory as these
 // contracts are shared from server to client
 
-export type ISseEventType = 'switch' | 'lock' | 'motion';
+export type ISseEventType = 'switch' | 'lock' | 'motion' | 'rule';
 
 export interface ISseEvent
 {
   deviceId: string;
   value: string;
+}
+
+export interface ISseRuleEvent {
+  appId: string;
 }
