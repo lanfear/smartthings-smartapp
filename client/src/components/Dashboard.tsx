3faa893e77d0b41067138a09f0c3d8aea5a75c5c
Joe Deming <joe@josephdeming.net>
2022-04-05 23:37:07 -0400

    useSWR properly to cache data in localstorage

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,13 +1,13 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {useTranslation} from 'react-i18next';
 import styled from 'styled-components';
 import {useParams} from 'react-router-dom';
-import {useLocalStorage} from 'use-hooks';
 import {Room as IRoom, SceneSummary} from '@smartthings/core-sdk';
 import {IDevice} from '../types/smartthingsExtensions';
 import Room from './Room';
 import {DeviceContextStore} from '../store/DeviceContextStore';
 import getLocation, {IResponseLocation} from '../operations/getLocation';
+import useSWR, {unstable_serialize as swrKeySerializer} from 'swr';
 
 const filteredRooms = ['DO NOT USE'];
 
@@ -52,62 +52,79 @@ const DashboardGridColumnHeader = styled.span`
     justify-content: center;
 `;
 
+const initialDashboardData: IResponseLocation = {
+  locationId: '',
+  rooms: [],
+  scenes: [],
+  switches: [],
+  locks: [],
+  motion: [],
+  rules: [],
+  apps: []
+};
+
+const sortRoom = (r: IRoom, l: IRoom): 1 | -1 | 0 => {
+  const rName = r.name?.toUpperCase() ?? ''; // ignore upper and lowercase
+  const lName = l.name?.toUpperCase() ?? ''; // ignore upper and lowercase
+  return rName < lName ? -1 : rName > lName ? 1 : 0;
+};
+
+const sortLabel = (r: IDevice, l: IDevice): 1 | -1 | 0 => {
+  const rName = r.label?.toUpperCase() ?? ''; // ignore upper and lowercase
+  const lName = l.label?.toUpperCase() ?? ''; // ignore upper and lowercase
+  return rName < lName ? -1 : rName > lName ? 1 : 0;
+};
+
+const sortScene = (r: SceneSummary, l: SceneSummary): 1 | -1 | 0 => {
+  const rName = r.sceneName?.toUpperCase() ?? ''; // ignore upper and lowercase
+  const lName = l.sceneName?.toUpperCase() ?? ''; // ignore upper and lowercase
+  return rName < lName ? -1 : rName > lName ? 1 : 0;
+};
+
+const getDashboard = async (location: string): Promise<IResponseLocation> => {
+  const locationData = await getLocation(location);
+  locationData.rooms = locationData.rooms?.sort(sortRoom).filter(r => !filteredRooms.includes(r.name as string)) ?? [];
+  locationData.scenes = locationData.scenes?.sort(sortScene) ?? [];
+  locationData.switches = locationData.switches?.sort(sortLabel) ?? [];
+  locationData.locks = locationData.locks?.sort(sortLabel) ?? [];
+  locationData.motion = locationData.motion?.sort(sortLabel) ?? [];
+  return locationData;
+};
+
+const getFallbackData = (locationId: string): IResponseLocation => {
+  const localStorageData = localStorage.getItem(swrKeySerializer(['locationData', locationId]));
+  return localStorageData ? JSON.parse(localStorageData) as IResponseLocation : {
+    ...initialDashboardData,
+    locationId
+  };
+};
+
 const Dashboard: React.FC = () => {
   const {t} = useTranslation();
 
-  const [dashboardData, setDashboardData] = useLocalStorage('locationData', {} as IResponseLocation);
-
   const routeInfo = useParams<{locationId: string}>();
   const locationId = routeInfo.locationId ?? '';
-
-  const sortRoom = (r: IRoom, l: IRoom): 1 | -1 | 0 => {
-    const rName = r.name?.toUpperCase() ?? ''; // ignore upper and lowercase
-    const lName = l.name?.toUpperCase() ?? ''; // ignore upper and lowercase
-    return rName < lName ? -1 : rName > lName ? 1 : 0;
-  };
-
-  const sortLabel = (r: IDevice, l: IDevice): 1 | -1 | 0 => {
-    const rName = r.label?.toUpperCase() ?? ''; // ignore upper and lowercase
-    const lName = l.label?.toUpperCase() ?? ''; // ignore upper and lowercase
-    return rName < lName ? -1 : rName > lName ? 1 : 0;
-  };
-
-  const sortScene = (r: SceneSummary, l: SceneSummary): 1 | -1 | 0 => {
-    const rName = r.sceneName?.toUpperCase() ?? ''; // ignore upper and lowercase
-    const lName = l.sceneName?.toUpperCase() ?? ''; // ignore upper and lowercase
-    return rName < lName ? -1 : rName > lName ? 1 : 0;
-  };
-
-  useEffect(() => {
-    const getDashboard = async (location: string): Promise<void> => {
-      const locationData = await getLocation(location);
-      locationData.rooms = locationData.rooms?.sort(sortRoom).filter(r => !filteredRooms.includes(r.name as string)) ?? [];
-      locationData.scenes = locationData.scenes?.sort(sortScene) ?? [];
-      locationData.switches = locationData.switches?.sort(sortLabel) ?? [];
-      locationData.locks = locationData.locks?.sort(sortLabel) ?? [];
-      locationData.motion = locationData.motion?.sort(sortLabel) ?? [];
-      setDashboardData(locationData);
-    };
-
-    void getDashboard(locationId);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); // ignore installedAppId
+  const {data: dashboardData, mutate: setDashboardData} = useSWR(['locationData', locationId], (_, l) => getDashboard(l), {
+    revalidateOnMount: true,
+    fallbackData: getFallbackData(locationId)
+  });
 
   const deleteRule = async (location: string, ruleId: string): Promise<void> => {
     // TODO: remove the app/id/rule/id delete endpoint and add one that takes location
     await fetch(`${process.env.REACT_APP_APIHOST as string}/${location}/rule/${ruleId}`, {method: 'DELETE'});
   };
 
+  const renderedDashboardData = dashboardData || initialDashboardData;
   return (
-    <DeviceContextStore value={{deviceData: dashboardData, setDeviceData: setDashboardData}}>
+    <DeviceContextStore value={{deviceData: renderedDashboardData, setDeviceData: setDashboardData}}>
       <DashboardTitle>
-        {dashboardData.locationId}
+        {renderedDashboardData.locationId}
       </DashboardTitle>
       <DashboardSubTitle>
         {t('dashboard.room.sectionName')}
       </DashboardSubTitle>
       <DashboardRoomGrid>
-        {dashboardData && dashboardData?.rooms?.map(r => (
+        {renderedDashboardData && renderedDashboardData?.rooms?.map(r => (
           <React.Fragment key={`room-${r.roomId as string}`}>
             <Room room={r} />
           </React.Fragment>
@@ -132,7 +149,7 @@ const Dashboard: React.FC = () => {
         <DashboardGridColumnHeader>
           {t('dashboard.scene.header.lastExecutedDate')}
         </DashboardGridColumnHeader>
-        {dashboardData && dashboardData?.scenes?.map(s => (
+        {renderedDashboardData && renderedDashboardData?.scenes?.map(s => (
           <React.Fragment key={`scene-${s.sceneId as string}`}>
             <span>
               {s.sceneName}
@@ -168,7 +185,7 @@ const Dashboard: React.FC = () => {
         <DashboardGridColumnHeader>
           {t('dashboard.rule.header.manage')}
         </DashboardGridColumnHeader>
-        {dashboardData && dashboardData.rules?.map(s => (
+        {renderedDashboardData && renderedDashboardData.rules?.map(s => (
           <React.Fragment key={`rules-${s.id}`}>
             <span>
               {s.name}
@@ -201,7 +218,7 @@ const Dashboard: React.FC = () => {
         <DashboardGridColumnHeader>
           {t('dashboard.rule.header.ownerId')}
         </DashboardGridColumnHeader>
-        {dashboardData && dashboardData.apps?.map(a => (
+        {renderedDashboardData && renderedDashboardData.apps?.map(a => (
           <React.Fragment key={`apps-${a.installedAppId}`}>
             <span>
               {a.displayName}
