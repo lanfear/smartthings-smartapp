9ca95c36fe1a605fced004ffb561d5bb7051b3da
Joe Deming <joe@josephdeming.net>
2022-04-12 22:59:59 -0400

    add factory to generate some standard styled-components

---
+++ client/src/factories/styleFactory.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const ControlIcon = styled.div`
+  font-size: larger;
+`;
+
+export const ControlStatus = styled.div`
+  font-size: smaller;
+  font-weight: 500;
+`;
+
+export const ControlContainer = styled.button`
+  height: 4rem;
+  width: 4rem;
+  display: flex;
+  flex: none;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
+  justify-content: space-evenly;
+  border: 1px solid gray;
+  border-radius: 4px;
+`;
