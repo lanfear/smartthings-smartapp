c4c4864cfc3676cc98b7a41887ad3e4411b79507
Joseph M. Deming <j.deming@techsmith.com>
2022-04-21 23:43:23 -0400

    add factory to create DnD objects and define types

---
+++ client/src/factories/dragAndDropFactory.ts
@@ -0,0 +1,44 @@
+import {DragSourceMonitor, DropTargetMonitor} from 'react-dnd';
+
+export enum IDragAndDropType {
+  App = 'app',
+  Power = 'power',
+  Device = 'device',
+  Rule = 'rule'
+}
+
+export interface IDragAndDropItem {
+  type: IDragAndDropType;
+  subtype?: string;
+  id: string;
+  displayName: string;
+}
+
+export interface IDragConfig {
+  type: IDragAndDropType;
+  item: IDragAndDropItem;
+  collect: (monitor: DragSourceMonitor) => { isDragging: boolean };
+}
+
+export interface IDropConfig {
+  accept: IDragAndDropType[];
+  drop: (item: IDragAndDropItem, monitor: DropTargetMonitor) => IDragAndDropItem;
+}
+
+export const createDragConfig = (type: IDragAndDropType, dragId: string, displayName: string, subtype?: string): IDragConfig => ({
+  type: type,
+  item: {
+    type: type,
+    subtype: subtype,
+    id: `${type}-${dragId}`,
+    displayName: displayName
+  },
+  collect: (monitor: DragSourceMonitor) => ({
+    isDragging: monitor.isDragging()
+  })
+});
+
+export const createDropConfig = (onDrop: (item: IDragAndDropItem, monitor: DropTargetMonitor) => IDragAndDropItem): IDropConfig => ({
+  accept: [IDragAndDropType.App, IDragAndDropType.Device, IDragAndDropType.Power, IDragAndDropType.Rule],
+  drop: onDrop
+});
