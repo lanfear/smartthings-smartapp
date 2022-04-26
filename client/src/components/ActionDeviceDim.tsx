a14993559ab5be5a1839254a959ae2d456119f70
Joseph M. Deming <j.deming@techsmith.com>
2022-04-25 20:55:18 -0400

    added stub for device dimming

---
+++ client/src/components/ActionDeviceDim.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {useDrop} from 'react-dnd';
+import global from '../constants/global';
+import {createDropConfig, IDragAndDropItem, IDragAndDropType} from '../factories/dragAndDropFactory';
+import {ControlContainer, ControlIcon, ControlStatus} from '../factories/styleFactory';
+import executeDeviceCommand from '../operations/executeDeviceCommand';
+import {useDeviceContext} from '../store/DeviceContextStore';
+
+const ActionDeviceDim: React.FC<IActionDeviceDimProps> = ({words}) => {
+  const {deviceData} = useDeviceContext();
+
+  const onDrop = async (item: IDragAndDropItem): Promise<IDragAndDropItem> => {
+    // component: 'main',
+    // capability: 'switchLevel',
+    // command: 'setLevel',
+    // arguments: [{integer: switchLevel}, {integer: rateLevel}]
+
+    if (item.type === IDragAndDropType.Device) {
+      // eslint-disable-next-line no-magic-numbers
+      await executeDeviceCommand(item.id, 'switchLevel', 'setLevel', 'main', [50, 5000]);
+    } else if (item.type === IDragAndDropType.Power) {
+      const roomSwitches = deviceData.switches.filter(d => d.roomId === item.id);
+      // TODO: this can be a single call if we expose the API properly
+      await Promise.all(roomSwitches.map(s => executeDeviceCommand(s.deviceId, 'switchLevel', 'setLevel', JSON.stringify([{integer: 50}, {integer: 5000}]))));
+    }
+    return item;
+  };
+
+  const [collectedProps, drop] = useDrop(() => createDropConfig(onDrop, [IDragAndDropType.Power, IDragAndDropType.Device]));
+  
+  const leftControl = (
+    <ControlContainer
+      rgb={global.palette.control.rgb.inactive}
+      ref={drop}
+      {...collectedProps}
+    >
+      <ControlIcon>
+          🤖
+      </ControlIcon>
+      <ControlStatus>
+        {words}
+      </ControlStatus>
+    </ControlContainer>
+  );
+  
+  return leftControl;
+};
+
+export interface IActionDeviceDimProps {
+  words: string;
+}
+
+export default ActionDeviceDim;
--- client/src/components/DeviceControls.tsx
+++ client/src/components/DeviceControls.tsx
@@ -1,5 +1,6 @@
 import React, {ReactNode} from 'react';
 import styled from 'styled-components';
+import ActionDeviceDim from './ActionDeviceDim';
 import ActionDeviceOff from './ActionDeviceOff';
 import ActionDeviceOn from './ActionDeviceOn';
 
@@ -12,13 +13,16 @@ const DeviceControlsContainer = styled.div`
 const DeviceControls: React.FC<IDeviceControlsProps> = ({words}) => {
   const deviceControls: ReactNode[] = [];
 
+  // eslint-disable-next-line no-console
+  console.log(words);
+
   deviceControls.push(<ActionDeviceOn words="PowerOn" />);
   deviceControls.push(<ActionDeviceOff words="PowerOff" />);
   
   return (
     <DeviceControlsContainer>
       {deviceControls.map(c => c)}
-      {words}
+      <ActionDeviceDim words="Dim" />
       {deviceControls.map(c => c)}
     </DeviceControlsContainer>
   );
