52bb962cafe294a2438dba8b2ddbd475fd30f5e3
Joe Deming <joe@josephdeming.net>
2023-08-27 20:15:24 -0400

    add smartAppContextStore for redis

---
+++ server/provider/smartAppContextStore.ts
@@ -0,0 +1,27 @@
+import {ContextRecord, ContextStore} from '@smartthings/smartapp';
+import {createClient} from 'redis';
+
+// TODO: this should take the appid as an arg & constructor
+const appContextPrefix = `st-appcontext-${process.env.ENV_TYPE}-`;
+const redisContextStore = createClient({
+  url: process.env.REDIS_SERVER
+});
+
+type RedisContextRecord = ContextRecord & Record<string, any>;
+
+const store: ContextStore = {
+  get: async installedAppId => {
+    await redisContextStore.connect();
+    const appRecord = JSON.parse(await redisContextStore.get(`${appContextPrefix}${installedAppId}`)) as RedisContextRecord;
+    await redisContextStore.disconnect();
+    return appRecord;
+  },
+  put: async contextRecord => {
+    await redisContextStore.connect();
+    await redisContextStore.set(`${appContextPrefix}${contextRecord.installedAppId}`, JSON.stringify(contextRecord));
+    await redisContextStore.disconnect();
+    return contextRecord;
+  }
+};
+
+export default store;
