74fb8dd20ac2c66e2614de60ab19443b785c5a91
Joe Deming <joe@josephdeming.net>
2023-09-03 21:40:52 -0400

    spruced up the rule mgmt dashboard to facilitate id and cleanup of rogue rules

--- client/src/components/DashboardRules.tsx
+++ client/src/components/DashboardRules.tsx
@@ -5,77 +5,113 @@ import styled from 'styled-components';
 import global from '../constants/global';
 import {DashboardTitle, DashboardGridColumnHeader} from '../factories/styleFactory';
 import {useDeviceContext} from '../store/DeviceContextStore';
-import {DeviceContext, IRule} from '../types/sharedContracts';
+import {IApp} from '../types/sharedContracts';
 
 const DashboardRuleGrid = styled.div`
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: 1fr;
     gap: ${global.measurements.dashboardGridGap};
     grid-auto-rows: minmax(100px, auto);
+`;
+
+const DashboardRuleLineItemGrid = styled.div`
+    display: grid;
+    grid-template-areas:
+      "name id   status app  manage"
+      "rule rule rule   rule rule";
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: 1fr max-content;
+    gap: ${global.measurements.dashboardGridGap};
+`;
+
+const DashboardRuleHeader = styled(DashboardGridColumnHeader) <{ gridArea: string }>`
+  grid-area: ${props => props.gridArea};
+`;
+
+const DashboardRuleContent = styled.span<{ gridArea: string }>`
+  grid-area: ${props => props.gridArea};
+`;
+
+const DashboardRuleName = styled.span<{ matchesInstalledApp: boolean }>`
+  grid-area: 'name';
+  color: ${props => props.matchesInstalledApp ? 'green' : 'red'};
+`;
+
+const DashboardRuleDataContent = styled(DashboardRuleContent)`
+  grid-area: ${props => props.gridArea};
+  font-family: monospace;
+  white-space: pre;
+  overflow: hidden;
 `;
 
 const DashboardRules: React.FC = () => {
   const {t} = useTranslation();
   const {deviceData} = useDeviceContext();
+  // const [existingAppList, setExistingAppList] = React.useState<Record<string, string>>({});
 
   const routeInfo = useParams<{locationId: string}>();
   const locationId = routeInfo.locationId ?? ''; // empty location id should not happen
 
+  const findAppMatchingRule = (ruleName: string): IApp|undefined => deviceData.apps.find(a => !!ruleName.match(new RegExp(`.*${a.installedAppId}.*`, 'i')));
+
   const deleteRule = async (location: string, ruleId: string): Promise<void> => {
-    await fetch(`${process.env.REACT_APP_APIHOST as string}/${location}/rule/${ruleId}`, {method: 'DELETE'});
+    await fetch(`${process.env.REACT_APP_APIHOST as string}/location/${location}/rule/${ruleId}`, {method: 'DELETE'});
   };
 
-  const findRoomName = (rule: IRule): string | undefined => {
-    const motionDevice = deviceData.motion.find(m =>
-      rule.ruleSummary?.motionSensors.find((mm: DeviceContext): boolean => m.deviceId === mm.deviceId)
-    );
-    const room = deviceData.rooms.find(r => r.roomId === motionDevice?.roomId);
-    return room?.name;
-  };
-
+  /* eslint-disable no-undefined */
   return (
     <>
       <DashboardTitle>
         {locationId}
       </DashboardTitle>
       <DashboardRuleGrid>
-        <DashboardGridColumnHeader>
-          {t('dashboard.rule.header.name')}
-        </DashboardGridColumnHeader>
-        <DashboardGridColumnHeader>
-          {t('dashboard.rule.header.ruleId')}
-        </DashboardGridColumnHeader>
-        <DashboardGridColumnHeader>
-          {t('dashboard.rule.header.status')}
-        </DashboardGridColumnHeader>
-        <DashboardGridColumnHeader>
-          {t('dashboard.rule.header.ownerId')}
-        </DashboardGridColumnHeader>
-        <DashboardGridColumnHeader>
-          {t('dashboard.rule.header.manage')}
-        </DashboardGridColumnHeader>
+        <DashboardRuleLineItemGrid>
+          <DashboardRuleHeader gridArea="name">
+            {t('dashboard.rule.header.name')}
+          </DashboardRuleHeader>
+          <DashboardRuleHeader gridArea="id">
+            {t('dashboard.rule.header.ruleId')}
+          </DashboardRuleHeader>
+          <DashboardRuleHeader gridArea="status">
+            {t('dashboard.rule.header.status')}
+          </DashboardRuleHeader>
+          <DashboardRuleHeader gridArea="app">
+            {t('dashboard.rule.header.ownerId')}
+          </DashboardRuleHeader>
+          <DashboardRuleHeader gridArea="manage">
+            {t('dashboard.rule.header.manage')}
+          </DashboardRuleHeader>
+        </DashboardRuleLineItemGrid>
         {deviceData && deviceData.rules?.map(s => (
           <React.Fragment key={`rules-${s.id}`}>
-            <span>
-              {s.name}
-            </span>
-            <span>
-              {s.id}
-            </span>
-            <span>
-              {s.executionLocation}
-            </span>
-            <span>
-              {findRoomName(s)}
-            </span>
-            <button onClick={() => deleteRule(locationId, s.id)}>
-            DELETE
-            </button>
+            <DashboardRuleLineItemGrid>
+              <DashboardRuleName matchesInstalledApp={!!findAppMatchingRule(s.name)}>
+                {s.name}
+              </DashboardRuleName>
+              <DashboardRuleContent gridArea="id">
+                {s.id}
+              </DashboardRuleContent>
+              <DashboardRuleContent gridArea="status">
+                {s.executionLocation}
+              </DashboardRuleContent>
+              <DashboardRuleContent gridArea="app">
+                {findAppMatchingRule(s.name)?.displayName ?? '(rogue rule)'}
+              </DashboardRuleContent>
+              <DashboardRuleContent gridArea="manage">
+                <button onClick={() => deleteRule(locationId, s.id)}>
+                  DELETE
+                </button>
+              </DashboardRuleContent>
+              <DashboardRuleDataContent gridArea="rule">
+                {JSON.stringify(s, undefined, 2)}
+              </DashboardRuleDataContent>
+            </DashboardRuleLineItemGrid>
           </React.Fragment>
         ))}
       </DashboardRuleGrid>
     </>
   );
+  /* eslint-enable no-undefined */
 };
 
 export default DashboardRules;
