e9fd1003a76974aba1adaaba718db213cc3b6f99
Joe Deming <joe@josephdeming.net>
2026-01-09 20:11:00 -0500

    ok app.Test.tsx is happy

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,12 @@
 import {render, screen} from '@testing-library/react';
 import App from './App';
 
+/* eslint-disable @typescript-eslint/no-explicit-any, vars-on-top, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access  */
+
 // if we care someday we can get types for jest or whatever
 declare global {
-  var test: any, expect: any;
+  var test: any;
+  var expect: any;
 }
 
 test('renders learn react link', () => {
