2d9cb3b7b58e50346d5b60054eb94138d1864b7d
Joe Deming <joe@josephdeming.net>
2026-01-09 19:47:51 -0500

    fixup! update eslint and related packages, and nest tsconfig

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,59 +1,48 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import getTscLintingConfig from "eslint-config-techsmith";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [{
-    ignores: ["**/node_modules/", "**/data/", "**/build/"],
-}, ...compat.extends("techsmith/es6", "techsmith/react"), {
+export default [
+  ...getTscLintingConfig(
+    ['**/node_modules/', '**/data/', '**/build/'],
+    {
+      process: "readonly",
+    },
+  ),
+  {
     languageOptions: {
-        globals: {
-            process: "readonly",
-        },
-        ecmaVersion: 2020,
-        sourceType: "script",
-        parserOptions: {
-            project: "./tsconfig.json",
-        },
+      ecmaVersion: 2020,
+      sourceType: "script",
+      // parserOptions: {
+      //   project: "./tsconfig.json",
+      // },
     },
-
+  },
+  {
     rules: {
-        "no-nested-ternary": "off",
-        "no-console": "warn",
-        indent: ["error", 2, {
-            SwitchCase: 1,
-        }],
+      "no-nested-ternary": "off",
+      "no-console": "warn",
+      indent: "off",
+      "@stylistic/indent": ["error", 2, {
+        SwitchCase: 1,
+      }],
     },
-}, ...compat.extends("techsmith/ts", "techsmith/react-ts").map(config => ({
-    ...config,
+  },
+  {
     files: ["**/*.ts", "**/*.tsx"],
-})), {
-    files: ["**/*.ts", "**/*.tsx"],
-
     rules: {
-        "no-nested-ternary": "off",
-        indent: "off",
-        "no-console": "warn",
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/indent": ["error", 2, {
-            SwitchCase: 1,
-
-            FunctionDeclaration: {
-                parameters: "first",
-            },
-
-            FunctionExpression: {
-                parameters: "first",
-            },
-        }],
+      strict: "off",
+      "no-nested-ternary": "off",
+      "no-console": "warn",
+      indent: "off",
+      "@stylistic/indent": ["error", 2, {
+        SwitchCase: 1,
+        FunctionDeclaration: {
+          parameters: "first",
+        },
+        FunctionExpression: {
+          parameters: "first",
+        },
+      }],
+      "@typescript-eslint/no-non-null-assertion": "off",
     },
-}];
-
+  },
+];
