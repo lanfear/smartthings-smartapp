f83e0f8f48a543fb5320dee2a967a66689c9ffc6
Joe Deming <j.deming@techsmith.com>
2026-01-11 15:19:37 -0500

    add singleton client instance

---
+++ server/provider/smartThingsClient.ts
@@ -0,0 +1,9 @@
+import {BearerTokenAuthenticator, SmartThingsClient} from '@smartthings/core-sdk';
+import {Nullable} from 'index';
+
+// singleton client instance
+let stClient: Nullable<SmartThingsClient> = null;
+
+const getSmartThingsClient = (): SmartThingsClient => stClient ?? (stClient = new SmartThingsClient(new BearerTokenAuthenticator(process.env.CONTROL_API_TOKEN!)));
+
+export default getSmartThingsClient;
